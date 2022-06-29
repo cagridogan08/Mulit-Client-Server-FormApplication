19f83cbba55bd22ac2b43fae4654f65f803a78b9
cagridogan08 <cagridogan08@gmail.com>
2022-06-29 17:34:23 +0300

    Update SocketClientFormApplication.sln, App.config, and 25 more files...

---
+++ SocketClientFormApplication/SocketClientFormApplication/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace SocketClientFormApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnSendData = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(249, 39);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(295, 202);
+            this.txtMessage.TabIndex = 0;
+            this.txtMessage.TextChanged += new System.EventHandler(this.txtMessage_TextChanged);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(182, 329);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnSendData
+            // 
+            this.btnSendData.Enabled = false;
+            this.btnSendData.Location = new System.Drawing.Point(559, 329);
+            this.btnSendData.Name = "btnSendData";
+            this.btnSendData.Size = new System.Drawing.Size(75, 23);
+            this.btnSendData.TabIndex = 2;
+            this.btnSendData.Text = "Send";
+            this.btnSendData.UseVisualStyleBackColor = true;
+            this.btnSendData.Click += new System.EventHandler(this.btnSendData_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(709, 388);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(75, 23);
+            this.btnDisconnect.TabIndex = 5;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Location = new System.Drawing.Point(588, 39);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Lambda State";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(61, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Lambda";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(6, 19);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(64, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Lambda";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnSendData);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtMessage);
+            this.Name = "Form1";
+            this.Text = "Client ";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnSendData;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SocketClientFormApplication/SocketClientFormApplication/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+namespace SocketClientFormApplication
+{
+    public partial class Form1 : Form
+    {
+        //private byte[] buffer;
+        Socket clientSocket;
+        IPAddress address = null;
+        public Form1()
+        {
+            InitializeComponent();
+            btnSendData.Text = "Random Generate";
+            btnSendData.Enabled = true;
+        }
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            directConnect();
+
+           
+        }
+        private void directConnect()
+        {
+            clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            string ip = "127.0.0.1";
+            var endpoint = new IPEndPoint(IPAddress.Parse(ip), 23000);
+            clientSocket.BeginConnect(endpoint, ConnectCallback, null);
+           // ReceiveData();
+        }
+
+        private void btnSendData_Click(object sender, EventArgs e)
+        {
+          Random random = new Random();
+            int sayi = random.Next();
+            SendString(sayi.ToString());
+            //txtMessage.Text=sayi.ToString();
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            clientSocket.Close();
+        }
+
+        private void txtMessage_TextChanged(object sender, EventArgs e)
+        {
+            //byte []buffer = new byte[1024];
+
+            //string data = txtMessage.Text;
+            //buffer = Encoding.ASCII.GetBytes(data);
+            //clientSocket.BeginSend(buffer, 0, buffer.Length, SocketFlags.None, SendCallBack, null);
+            SendString(txtMessage.Text);
+            //clientSocket.Send(buffer);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            //byte[] buffer = new byte[1024];
+            //buffer = Encoding.UTF8.GetBytes(checkBox1.Checked.ToString());
+            //clientSocket.BeginSend(buffer, 0, buffer.Length, SocketFlags.None, SendCallBack, null);
+            SendString(checkBox1.Checked.ToString());
+        }
+        private  void SendString(string text)
+        {
+            byte[] buffer = Encoding.ASCII.GetBytes(text);
+            clientSocket.Send(buffer, 0, buffer.Length, SocketFlags.None);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                checkBox1.Checked = false;
+            }
+            else
+            {
+                checkBox1.Checked=true;
+            }
+        }
+        private void ReceiveCallBack(IAsyncResult AR)
+        {
+            var buffer = new byte[1024];
+            try
+            {
+                while (true) { 
+                ReceiveData();
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void ConnectCallback(IAsyncResult AR)
+        {
+            
+            try
+            {
+                Thread.Sleep(100);
+                clientSocket.EndConnect(AR);
+                byte[] buffer = new byte[clientSocket.ReceiveBufferSize];
+                clientSocket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, ReceiveCallBack, null);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+        private void SendCallBack(IAsyncResult Ar)
+        {
+            try
+            {
+                clientSocket.EndSend(Ar);
+            }
+            catch(Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);
+            }
+        }
+        private void ReceiveData()
+        {
+            var buffer = new byte[2048];
+            int received = clientSocket.Receive(buffer, SocketFlags.None);
+            if (received == 0) return;
+            var data = new byte[received];
+            Array.Copy(buffer, data, received);
+            string text = Encoding.ASCII.GetString(data);
+
+            int number;
+            bool lab;
+            if (int.TryParse(text, out number))
+            {
+                Invoke((Action)delegate
+                {
+                    btnSendData.Text = number.ToString();
+                });
+
+            }
+            else if (bool.TryParse(text, out lab))
+            {
+                Invoke((Action)delegate
+                {
+                    checkBox1.Checked = lab;
+                });
+
+            }
+            else
+            {
+
+                Invoke((Action)delegate
+                {
+                    txtMessage.Text = text;
+                });
+
+            }
+        }
+        private void AppendToTextBox(string a)
+        {
+
+        }
+    }
+}
