3048d6ccb5db8c95b8570fc1233fe787e8ee7a73
cagridogan08 <cagridogan08@gmail.com>
2022-06-29 17:34:28 +0300

    Update SocketServerFormApplication.sln, App.config, and 25 more files...

---
+++ SocketServerFormApplication/SocketServerFormApplication/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace SocketServerFormApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.btnRandom = new System.Windows.Forms.Button();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.chcLambda = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(38, 188);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 0;
+            this.btnCreate.Text = "Create Server";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Location = new System.Drawing.Point(22, 23);
+            this.txtStatus.Multiline = true;
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(137, 145);
+            this.txtStatus.TabIndex = 1;
+            // 
+            // btnRandom
+            // 
+            this.btnRandom.Location = new System.Drawing.Point(323, 188);
+            this.btnRandom.Name = "btnRandom";
+            this.btnRandom.Size = new System.Drawing.Size(75, 23);
+            this.btnRandom.TabIndex = 2;
+            this.btnRandom.Text = "Random";
+            this.btnRandom.UseVisualStyleBackColor = true;
+            this.btnRandom.TextChanged += new System.EventHandler(this.btnRandom_TextChanged);
+            this.btnRandom.Click += new System.EventHandler(this.btnRandom_Click);
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(278, 23);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(186, 113);
+            this.txtMessage.TabIndex = 3;
+            this.txtMessage.TextChanged += new System.EventHandler(this.txtMessage_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.chcLambda);
+            this.groupBox1.Location = new System.Drawing.Point(536, 23);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Lambda State";
+            // 
+            // chcLambda
+            // 
+            this.chcLambda.AutoSize = true;
+            this.chcLambda.Checked = true;
+            this.chcLambda.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chcLambda.Location = new System.Drawing.Point(7, 20);
+            this.chcLambda.Name = "chcLambda";
+            this.chcLambda.Size = new System.Drawing.Size(64, 17);
+            this.chcLambda.TabIndex = 0;
+            this.chcLambda.Text = "Lambda";
+            this.chcLambda.UseVisualStyleBackColor = true;
+            this.chcLambda.CheckedChanged += new System.EventHandler(this.chcLambda_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(748, 321);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.btnRandom);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.btnCreate);
+            this.Name = "Form1";
+            this.Text = "Server";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.TextBox txtStatus;
+        private System.Windows.Forms.Button btnRandom;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox chcLambda;
+    }
+}
+
---
+++ SocketServerFormApplication/SocketServerFormApplication/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+namespace SocketServerFormApplication
+{
+ 
+    public partial class Form1 : Form
+    {
+        private static readonly Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private static readonly List<Socket> clientSockets = new List<Socket>();
+        private const int BUFFER_SIZE = 2048;
+        private const int PORT = 23000;
+        private static readonly byte[] buffer = new byte[BUFFER_SIZE];
+        public static ManualResetEvent allDone = new ManualResetEvent(false);
+        public Form1()
+        {
+            InitializeComponent();
+            StartServer();
+
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+           //
+            
+        }
+        private void StartServer()
+        {
+                //serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                try
+                {
+                    //allDone.Reset();
+                    serverSocket.Bind(new IPEndPoint(IPAddress.Any, PORT));
+                    serverSocket.Listen(10);
+                    serverSocket.BeginAccept(AcceptCallBack,serverSocket);
+                    //allDone.WaitOne();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+        }
+        private  void AcceptCallBack(IAsyncResult AR)
+        {
+
+            Socket socket;
+
+            try
+            {
+                socket = serverSocket.EndAccept(AR);
+            }
+            catch (ObjectDisposedException) // I cannot seem to avoid this (on exit when properly closing sockets)
+            {
+                return;
+            }
+
+            clientSockets.Add(socket);
+            
+            string xx = txtMessage.Text;
+            byte[] data1 = Encoding.UTF8.GetBytes(xx);
+            //socket.Send(data1);
+            byte[] data2 = Encoding.UTF8.GetBytes(chcLambda.Checked.ToString());
+            //socket.Send(data2);
+            byte[] data3 = Encoding.UTF8.GetBytes(btnRandom.Text);
+            //socket.Send(data3);
+            socket.BeginSend(data1, 0, data1.Length, SocketFlags.None, SendCallBack, socket);
+            Thread.Sleep(500);
+            socket.BeginSend(data2, 0, data2.Length, SocketFlags.None, SendCallBack, socket);
+            Thread.Sleep(500);
+            socket.BeginSend(data3, 0, data3.Length, SocketFlags.None, SendCallBack, socket);
+            Thread.Sleep(500);
+            socket.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceiveCallBack, socket);
+            AppendToTextBox("Client connected, waiting for request...");
+            AppendToTextBox(socket.ToString() + "IP Endpoint : " + socket.RemoteEndPoint.ToString());
+            serverSocket.BeginAccept(AcceptCallBack, null);
+            
+        }
+
+       
+        private  void ReceiveCallBack(IAsyncResult Ar)
+        {
+            Socket clientSocket = (Socket)Ar.AsyncState;
+            int data_size;
+            
+
+            try
+            {
+                data_size = clientSocket.EndReceive(Ar);
+            }
+            catch(Exception ex)
+            {
+                AppendToTextBox("Client Disconnected");
+                clientSocket.Close();
+                clientSockets.Remove(clientSocket);
+                return;
+            }
+            byte [] received_Buffer = new byte[data_size];
+            Array.Copy(buffer, received_Buffer, data_size);
+            string data = Encoding.ASCII.GetString(received_Buffer);
+            int number;
+            bool lab;
+            if (int.TryParse(data, out number))
+            {
+                Invoke((Action)delegate
+                {
+                    btnRandom.Text = number.ToString();
+                   // txtMessage.Text=number.ToString();
+                    //clientSocket.Send(Encoding.UTF8.GetBytes(data));
+                });
+                
+
+            }
+            else if (bool.TryParse(data, out lab))
+            {
+                Invoke((Action)delegate
+                {
+                    chcLambda.Checked = lab;
+                    //txtMessage.Text = lab.ToString();
+                });
+
+            }
+            else
+            {
+
+                Invoke((Action)delegate
+                {
+                    txtMessage.Text = data;
+                });
+
+            }
+
+            clientSocket.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceiveCallBack, clientSocket);
+
+
+        }
+        private void SendAllClients(byte[] data)
+        {
+            foreach (Socket client in clientSockets)
+            {
+                client.Send(data);
+            }
+        }
+         
+        private  void AppendToTextBox(string a)
+        {
+            Invoke((Action)delegate
+            {
+                txtStatus.Text+=a+"\r\n";
+            });
+
+        }
+
+        private void btnRandom_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int xx = random.Next();
+            txtMessage.Text = xx.ToString();
+        }
+
+        private void txtMessage_TextChanged(object sender, EventArgs e)
+        {
+            //Array.Clear(buffer, 0, buffer.Length);
+            //buffer = Encoding.UTF8.GetBytes(txtMessage.Text);
+            byte[] data = new byte[BUFFER_SIZE];
+            data = Encoding.UTF8.GetBytes(txtMessage.Text);
+            SendAllClients(data);
+            ////clientSocket.BeginSend(buffer, 0, buffer.Length, SocketFlags.None, SendCallBack, null);
+
+        }
+
+        private void chcLambda_CheckedChanged(object sender, EventArgs e)
+        {
+            byte[] data = new byte[BUFFER_SIZE];
+            data = Encoding.UTF8.GetBytes(chcLambda.Checked.ToString());
+            SendAllClients(data);
+        }
+
+        private void btnRandom_TextChanged(object sender, EventArgs e)
+        {
+            byte[] data = new byte[BUFFER_SIZE];
+            data = Encoding.UTF8.GetBytes(btnRandom.Text.ToString());
+            SendAllClients(data);
+        }
+        private void SendCallBack(IAsyncResult AR)
+        {
+            Socket clientSocket = (Socket)AR.AsyncState;
+            try
+            {
+               clientSocket.EndSend(AR);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
